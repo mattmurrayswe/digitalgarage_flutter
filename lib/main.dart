2aba2b451dd7c81367282d51d472b32cc8295ddf
Matheus Murray <mattmurrayswe@gmail.com>
2025-06-29 18:43:42 -0300

    feat: router

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'models/scanned_tickets.dart';
 import 'models/ticket.dart';
 import 'app.dart';
 
@@ -13,8 +14,10 @@ void main() async {
   Hive.init(dir.path);
 
   Hive.registerAdapter(TicketAdapter());
+  Hive.registerAdapter(ScannedTicketsAdapter());
 
   await Hive.openBox<Ticket>('tickets');
+  await Hive.openBox<ScannedTickets>('scanned_tickets');
 
   runApp(const MyApp());
 }
